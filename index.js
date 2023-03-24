fa33c371f74128887b7581d9938b65081c25fca4
RossannaAriza <rossanna.ariza@gmail.com>
2023-03-24 13:59:34 -0500

    organizing files

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-  // ...
+//Create an mdLinks function, it receives two parameters, the first is a "URL", and the second option can be "--stats, --validate or --stats --validate", the last being a boolean value.
+const mdLinks = (path, options) => {
+//return new promise
+return new Promise((resolve, reject)=>{});
+}
+
+
+module.exports = {
+  mdLinks
 };
